8600a497f969ce8dce2b97e5a9477539e7129ad8
Djigbo <32855280+Djigbo@users.noreply.github.com>
2018-10-30 12:54:02 +0100

    Itinial commit

---
+++ CLDateTools.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tools
+{
+    public static class CLDateTools
+    {
+        /// <summary>
+        /// This string array holds the date formats that are valid
+        /// </summary>
+        /// <remarks>These formats are used by <see cref="CLDateTools.ValidateDateString(string)"/> </remarks>
+        private static readonly string[] Formats =
+        {
+            "M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
+            "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
+            "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
+            "M/d/yyyy h:mm", "M/d/yyyy h:mm",
+            "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm",
+            "dd/MM/yyyy", "d/M/yyyy"
+        };
+
+        public static TimeSpan? GetDiffStringDate(string fromdate)
+        {
+            var dtFromDate = ValidateDateString(fromdate);
+            var dtToDate = DateTime.Now;
+            if (dtFromDate.HasValue)
+            {
+                TimeSpan timeSpan = dtToDate - dtFromDate.Value;
+                return timeSpan;
+            }
+
+            return null;
+        }
+
+        public static TimeSpan? GetDiffStringDate(string fromdate, string todate)
+        {
+            var dtFromDate = ValidateDateString(fromdate);
+            var dtToDate = CLDateTools.ValidateDateString(todate);
+            if (dtFromDate.HasValue && dtToDate.HasValue)
+            {
+                TimeSpan timeSpan = dtToDate.Value - dtFromDate.Value;
+                return timeSpan;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Given a date, this function returns the age according to the current date.
+        /// </summary>
+        /// <param name="givenDate"></param>
+        /// <returns>The age if success otherwise -1</returns>
+        /// <remarks>-1 is returned when parameter givenDate is null.</remarks>
+        /// <seealso cref="DateTime"/>
+        public static int GetAgeFromDate(DateTime givenDate)
+        {
+            if (givenDate == null)
+            {
+                return -1;
+            }
+
+            var dateNow = DateTime.Now;
+            var age = dateNow.Year - givenDate.Year;
+
+            if (dateNow.Month < givenDate.Month || (dateNow.Month == givenDate.Month && dateNow.Day < givenDate.Day))
+                age--;
+
+            return age;
+        }
+
+        public static bool ValidateYearString(string yearToValidate)
+        {
+            if (string.IsNullOrEmpty(yearToValidate))
+            {
+                return false;
+            }
+
+            DateTime dateTimeOut;
+            return DateTime.TryParseExact(yearToValidate, "yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTimeOut);
+        }
+
+        /// <summary>
+        /// Validate a date given a string parameter that must be conformed to a format.
+        /// </summary>
+        /// <param name="dateToValidate">The string to validate.</param>
+        /// <returns>dateTime is the date is valid, otherwise null.</returns>
+        /// <remarks>
+        /// null is eturned when parameter dateToValidate is null or empty, <see cref="DateTime.TryParseExact"/> returns false.
+        /// </remarks>
+        public static DateTime? ValidateDateString(string dateToValidate)
+        {
+            DateTime dateTimeOut;
+
+            if (string.IsNullOrEmpty(dateToValidate) || string.IsNullOrWhiteSpace(dateToValidate))
+            {
+                return null;
+            }
+
+            if (!DateTime.TryParseExact(dateToValidate, Formats, null, DateTimeStyles.None, out dateTimeOut))
+            {
+                return null;
+            }
+
+            DateTime? objDataTime = dateTimeOut;
+            return objDataTime;
+        }
+    }
+}
---
+++ CLFileTools.cs
@@ -0,0 +1,156 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ClFileTools.cs" company="Ideal Software">
+//     Copyright (c) Ideal Software. All rights reserved.
+// </copyright>
+// <author>Stephane Amoa</author>
+//-----------------------------------------------------------------------
+
+namespace Tools
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// This class has helper functions related to file.
+    /// </summary>
+    /// <remarks>Used for file operations.</remarks>
+    public static class CLFileTools
+    {
+
+        /// <summary>
+        /// Build a list of objects from stream from a string
+        /// </summary>
+        /// <param name="xmlStream">The stream</param>
+        /// <param name="fromString">Where to start to get informations </param>
+        /// <remarks>The stream must be open and valid.</remarks>
+        /// <returns>An object list or null</returns>
+        public static List<object> BuildDatafromStream(Stream xmlStream, string fromString)
+        {
+            if (xmlStream == null)
+            {
+                return null;
+            }
+
+            var xdoc = XDocument.Load(xmlStream);
+            List<object> lstObj = null;
+            var datatofind = xdoc.Root.Elements(fromString);
+            if ((datatofind != null) && (datatofind.Any()))
+            {
+                lstObj = new List<object>();
+                foreach (var obj in datatofind)
+                {
+                    lstObj.Add(obj);
+                }
+            }
+
+            return lstObj;
+        }
+
+        public static List<object> BuildDescendantsDataFromStream(Stream xmlStream, string fromString, string selectstring)
+        {
+            if (xmlStream == null)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(fromString))
+            {
+                return null;
+            }
+
+            var xdoc = XDocument.Load(xmlStream);
+            List<object> lstObj = null;
+            var datatofind = xdoc.Root.Elements(fromString).Descendants(selectstring);
+            {
+                lstObj = new List<object>();
+                foreach (var obj in datatofind)
+                {
+                    lstObj.Add(obj);
+                }
+            }
+
+            return lstObj;
+        }
+
+        /// <summary>
+        /// Build a list of objects from stream from a string
+        /// </summary>
+        /// <param name="xmlStream">The stream</param>
+        /// <param name="fromString">Where to start to get informations </param>
+        /// <param name="whereString">Criteria</param>
+        /// <param name="selectString">The selected string</param>
+        /// <returns>The list of object if found otherwise null.</returns>
+        /// <remarks>
+        /// Null is returned when the stream is null, when nothing was found.
+        /// </remarks>
+        public static List<object> BuildDatafromStream(Stream xmlStream, string fromString, string whereString, string selectString)
+        {
+            if (xmlStream == null)
+            {
+                return null;
+            }
+
+            var xdoc = XDocument.Load(xmlStream);
+            List<object> lstObj = null;
+            var datatofind = from continent in xdoc.Descendants(fromString).Where(continent => (string)continent.Attribute("name") == whereString)
+                             select continent.Descendants(selectString);
+
+            if ((datatofind != null) && (datatofind.Any()))
+            {
+                lstObj = new List<object>();
+                foreach (var obj in datatofind)
+                {
+                    var size = obj.Count();
+                    for (int i = 0; i < size; i++)
+                    {
+                        lstObj.Add(obj.ElementAt(i));
+                    }
+                }
+            }
+
+            return lstObj;
+        }
+
+        /// <summary>
+        /// This a dummy function used for testing.
+        /// </summary>
+        /// <param name="xmlStream">The stream, must be valid</param>
+        /// <param name="fromString">Criteria to start</param>
+        /// <param name="whereString">Criteria condition</param>
+        /// <param name="selectString">The string to select</param>
+        /// <returns>The list of object if found otherwise null</returns>
+        /// <remarks>
+        /// Null is returned when the stream is null, when nothing was found.
+        /// </remarks>
+        //public static List<object> DummyBuildDatafromStream(Stream xmlStream, string fromString, string whereString, string selectString)
+        //{
+        //    if (xmlStream == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    var xdoc = XDocument.Load(xmlStream);
+        //    List<object> lstObj = null;
+        //    var datatofind = from continent in xdoc.Descendants(fromString).Where(continent => (string)continent.Element("Location").Attribute("town") == whereString)
+        //                     select continent.Descendants(selectString);
+
+        //    if ((datatofind != null) && (datatofind.Any()))
+        //    {
+        //        lstObj = new List<object>();
+        //        foreach (var obj in datatofind)
+        //        {
+        //            var size = obj.Count();
+        //            for (int i = 0; i < size; i++)
+        //            {
+        //                lstObj.Add(obj.ElementAt(i));
+        //            }
+        //        }
+        //    }
+
+        //    return lstObj;
+        //}
+    }
+}
+
